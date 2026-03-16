edc58b8b817cf1499449fde24a3aa5c6b40b6e4b
MommySuperior <projectx.py@gmail.com>
2026-03-16 10:26:25 +0100

    Add bootstraps and create histograms pre/post-COVID data

--- src/UNGD_samp_stats.py
+++ src/UNGD_samp_stats.py
@@ -9,7 +9,7 @@ UNGD_pre_covid = Path(__file__).parent.parent / "data" / "processed" / "UNGD_pre
 UNGD_post_covid = Path(__file__).parent.parent / "data" / "processed" / "UNGD_post_covid.csv"
 
 #function which automatically does bootstraping, for example see use below
-nboot = 1000 #change value of itterations
+nboot = 2000 #change value of itterations
 def bootstrap(x, nboot, statsfunc):
     x = np.array(x)
 
@@ -22,6 +22,9 @@ def bootstrap(x, nboot, statsfunc):
 
     return np.array(resampled_stats)
 
+#-----------------------------------------------------
+# UNGD_happiness summary, bootstrap, and histogram
+#-----------------------------------------------------
 df = pd.read_csv(UNGD_happiness)
 df = df[(df["year"] >= 2015) & (df["year"] <= 2025)]
 print("Dataset head:") # this can go
@@ -39,20 +42,6 @@ summary = df.groupby("year")["happiness_average"].agg(
 print("Range summary:")
 print(summary)
 
-# not sure if we're using the stuff below
-yr_2015 = df[df["year"] == "2015"]["happiness_average"]
-yr_2016 = df[df["year"] == "2016"]["happiness_average"]
-yr_2017 = df[df["year"] == "2017"]["happiness_average"]
-yr_2018 = df[df["year"] == "2018"]["happiness_average"]
-yr_2019 = df[df["year"] == "2019"]["happiness_average"]
-yr_2020 = df[df["year"] == "2020"]["happiness_average"]
-yr_2021 = df[df["year"] == "2021"]["happiness_average"]
-yr_2022 = df[df["year"] == "2022"]["happiness_average"]
-yr_2023 = df[df["year"] == "2023"]["happiness_average"]
-yr_2024 = df[df["year"] == "2024"]["happiness_average"]
-yr_2025 = df[df["year"] == "2025"]["happiness_average"]
-
-
 summary_bootstrap = bootstrap(summary, nboot, np.mean) # example of bootstrap function feel free to change var name
 
 # important stats for analysis, repeat for following data frames
@@ -65,5 +54,75 @@ print("Summary bootstrap std: \n", np.std(summary_bootstrap))
 plt.figure()
 plt.hist(summary_bootstrap, bins=50)
 plt.tight_layout()
-plt.savefig("Hedonometer/output/figures/UNGD_summary_bootstrap.png")
+plt.savefig("output/figures/UNGD_summary_bootstrap.png")
+plt.close()
+
+#-----------------------------------------------------
+# UNGD_pre_covid summary, bootstrap, and histogram
+#-----------------------------------------------------
+df_pre = pd.read_csv(UNGD_pre_covid)
+df_pre = df_pre[(df_pre["year"] >= 2015) & (df_pre["year"] <= 2019)]
+print("Pre-COVID head:") # this can go
+print(df_pre.head()) # this too
+print("Pre-COVID description:")
+print(df_pre.describe())
+
+summary_pre = df_pre.groupby("year")["happiness_average"].agg(
+    mean="mean",
+    median="median",
+    standard_deviation ="std",
+    occurrence="count" # how many times this year occurs in column "year"
+)
+
+print("Pre-COVID summary:")
+print(summary_pre)
+
+summary_pre_bootstrap = bootstrap(summary_pre, nboot, np.mean) # example of bootstrap function feel free to change var name
+
+# important stats for analysis, repeat for following data frames
+print("Summary mean: \n", np.mean(summary_pre))
+print("Summary bootstrap mean: \n", np.mean(summary_pre_bootstrap))
+print("Summary bootstrap median \n", np.median(summary_pre_bootstrap))
+print("Summary bootstrap std: \n", np.std(summary_pre_bootstrap))
+
+# histogram for the summary dataframe
+plt.figure()
+plt.hist(summary_pre_bootstrap, bins=50)
+plt.tight_layout()
+plt.savefig("output/figures/UNGD_pre_covid_bootstrap.png")
+plt.close()
+
+#-----------------------------------------------------
+# UNGD_post_covid summary, bootstrap, and histogram
+#-----------------------------------------------------
+df_post = pd.read_csv(UNGD_post_covid)
+df_post = df_post[(df_post["year"] >= 2020) & (df_post["year"] <= 2025)]
+print("Post-COVID head:") # this can go
+print(df_post.head()) # this too
+print("Post-COVID description:")
+print(df_post.describe())
+
+summary_post = df_post.groupby("year")["happiness_average"].agg(
+    mean="mean",
+    median="median",
+    standard_deviation ="std",
+    occurrence="count" # how many times this year occurs in column "year"
+)
+
+print("Post-COVID summary:")
+print(summary_post)
+
+summary_post_bootstrap = bootstrap(summary_post, nboot, np.mean) # example of bootstrap function feel free to change var name
+
+# important stats for analysis, repeat for following data frames
+print("Summary mean: \n", np.mean(summary_post))
+print("Summary bootstrap mean: \n", np.mean(summary_post_bootstrap))
+print("Summary bootstrap median \n", np.median(summary_post_bootstrap))
+print("Summary bootstrap std: \n", np.std(summary_post_bootstrap))
+
+# histogram for the summary dataframe
+plt.figure()
+plt.hist(summary_post_bootstrap, bins=50)
+plt.tight_layout()
+plt.savefig("output/figures/UNGD_post_covid_bootstrap.png")
 plt.close()
